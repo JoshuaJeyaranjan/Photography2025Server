1fe7b5d6fbb6ddcabe616d78b1b42b0a5ec46d48
Joshua Jeyaranjan <joshuajeyaranjan@macbookpro.lan>
2025-06-19 18:06:37 -0400

    undo blunder

--- db.js
+++ db.js
@@ -4,11 +4,11 @@ const knex = require('knex');
 const db = knex({
   client: 'mysql2',
   connection: {
-    host: process.env.DATABASE_HOST,
+    host: process.env.DB_HOST,
     port: 3306,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: {
       rejectUnauthorized: true, // Needed for PlanetScale
     },
